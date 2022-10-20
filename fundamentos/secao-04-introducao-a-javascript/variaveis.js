e9055bb7b40fd1c64a0a0af2b1acdb3fd9ada16e
Ana Carolina Mendes <carol@Anas-MacBook-Air.local>
2022-10-20 15:02:13 -0700

    javaScriptexercicios

---
+++ fundamentos/secao-04-introducao-a-javascript/variaveis.js
@@ -0,0 +1,14 @@
+const myName = "Carolina";
+const birthCity = "Sabará";
+let birthYear = 1985;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);;
+
+birthCity = "New York";
+console.log(birthCity);
+// TypeError: Assignment to constant variable.
