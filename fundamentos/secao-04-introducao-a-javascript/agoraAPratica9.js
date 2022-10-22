d38ea2ba799a6f41820e686dd80c92f8358dc914
Ana Carolina Mendes <carol@Anas-MacBook-Air.local>
2022-10-21 22:30:19 -0700

    javaScriptexercicios

---
+++ fundamentos/secao-04-introducao-a-javascript/agoraAPratica9.js
@@ -0,0 +1,18 @@
+let numbers = [];
+
+for(let index = 1; index<= 25; index += 1){
+    numbers.push(index); 
+}
+
+console.log (numbers);
+
+
+for (let index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index] / 2);
+  };
+
+
+
+// Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.
+
+
