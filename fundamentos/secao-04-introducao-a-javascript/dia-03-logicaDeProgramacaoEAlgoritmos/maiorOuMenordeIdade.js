de63f1083ec011313fa22d90df14bd4bbf3ffd9f
Ana Carolina Mendes <carol@Anas-MacBook-Air.local>
2022-10-24 16:08:05 -0700

    javaScriptexercicios-dia03

--- fundamentos/secao-04-introducao-a-javascript/dia-03-logicaDeProgramacaoEAlgoritmos/maiorOuMenordeIdade.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-logicaDeProgramacaoEAlgoritmos/maiorOuMenordeIdade.js
@@ -1,3 +1,14 @@
 // Desenvolva um algoritmo que verifica se a pessoa é maior ou menor de idade. Imprima no console seguindo o exemplo: “A pessoa é maior de idade”.
 // Bônus: Crie a condição utilizando operador ternário.
 
+PESQUISAR SOBRE OPERADOR TERNÁRIO
+
+// let mateus = 17;
+
+// if (mateus>=18) { 
+//     console.log("A pessoa é menor de idade")
+
+// }
+
+// mateus>=18 ? console.log("A pessoa é maior de idade")
+// : console.log("A pessoa é menor de idade")
