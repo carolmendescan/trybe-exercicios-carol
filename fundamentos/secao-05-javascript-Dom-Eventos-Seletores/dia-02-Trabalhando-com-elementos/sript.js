7358edc0450b93a36c666ebf77e1bcf14143501f
Ana Carolina Mendes <carol@Anas-MacBook-Air.local>
2022-10-31 11:53:09 -0700

    exercicios

--- fundamentos/secao-05-javascript-Dom-Eventos-Seletores/dia-02-Trabalhando-com-elementos/sript.js
+++ fundamentos/secao-05-javascript-Dom-Eventos-Seletores/dia-02-Trabalhando-com-elementos/sript.js
@@ -1,1 +1,18 @@
 const elementoOndeVoceEsta = document.getElementById("elementoOndeVoceEsta");
+
+
+const pai = document.getElementById("elementoOndeVoceEsta").parentElement;
+pai.style.color = 'red';
+
+const primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
+primeiroFilhoDoFilho.innerText = 'Adicionei um texto aqui!';
+
+const primeiroFilho = pai.firstElementChild;
+
+const primeiroFilho = elementoOndeVoceEsta.previousElementSibling;
+
+const textElement = elementoOndeVoceEsta.nextSibling;
+
+const terceiroFilho = elementoOndeVoceEsta.nextElementSibling;
+
+const terceiroFilho2 = pai.lastElementChild.previousElementSibling;
